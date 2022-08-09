05a90bda4941735456266accce8fc1fedc3321c0
Julia_DM <97304652+YuliyaDM@users.noreply.github.com>
2022-08-09 11:24:29 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,63 @@
+let i = 0;
+
+const sound = new Audio();
+sound.src = './sound/GTA_SA Mission Completed минус уши (BASSBOOSTED).mp4';
+
+window.addEventListener('mousemove', () => {
+    if (i <= 2){
+        setTimeout(() => {
+            sound.play();
+        }, 5000);
+        console.log('you focused on the window.')
+    }
+    i++
+})
+
+setTimeout(() => {
+    const image = document.createElement('img');
+    image.src = 'image/scream.png';
+    image.classList.add('scream1');
+    image.classList.add('scream');
+    console.log(image)
+    document.body.appendChild(image);
+}, 5000);
+
+setTimeout(() => {
+    const image = document.createElement('img');
+    image.src = 'image/scream.png';
+    image.classList.add('scream2');
+    image.classList.add('scream');
+    console.log(image)
+    document.body.appendChild(image);
+}, 7500);
+
+setTimeout(() => {
+    const image = document.createElement('img');
+    image.src = 'image/scream.png';
+    image.classList.add('scream3');
+    image.classList.add('scream');
+    console.log(image)
+    document.body.appendChild(image);
+}, 10000);
+
+setTimeout(() => {
+    const container = document.getElementsByClassName('container')[0];
+    container.style.visibility = 'hidden';
+}, 14000);
+
+setTimeout(() => {
+    const pre = document.getElementById('my_code');
+    document.body.style.background = 'white';
+    pre.style.visibility = 'visible';
+}, 16000);
+
+setTimeout(() => {
+    const slidans = document.querySelectorAll('.scream');
+    for (let i = 0; i < slidans.length; i++){
+        slidans[i].style.opacity = '0';
+    }
+}, 17500);
+
+setTimeout(() => {
+    document.body.style.overflowY = 'auto';
+}, 19500);
